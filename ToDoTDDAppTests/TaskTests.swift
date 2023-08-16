8b73b68b9830001820d8cee0d2564848b4bfd794
Zaki <menza.zm@gmail.com>
2023-08-16 10:20:17 +0600

    TDD tests for task model done

---
+++ ToDoTDDAppTests/TaskTests.swift
@@ -0,0 +1,43 @@
+//
+//  TaskTests.swift
+//  ToDoTDDAppTests
+//
+//  Created by Zaki on 15.08.2023.
+//
+
+import XCTest
+@testable import ToDoTDDApp
+
+final class TaskTests: XCTestCase {
+
+    func testInitTaskWithTitle() {
+        let task = Task(title: "Foo")
+        
+        XCTAssertNotNil(task)
+    }
+    
+    func testInitTaskWithTitleAndDescription() {
+        let task = Task(title: "Foo", description: "Bar")
+        
+        XCTAssertNotNil(task)
+    }
+    
+    func testWhenGivenTitleSetsTitle() {
+        let task = Task(title: "Bar")
+        
+        XCTAssertEqual(task.title, "Bar")
+    }
+    
+    func testWhenGivenDescriptionSetsDescription() {
+        let task = Task(title: "Foo", description: "Bar")
+        
+        XCTAssertTrue(task.description == "Bar")
+    }
+    
+    func testTaskInitWithData() {
+        let task = Task(title: "Foo")
+        
+        XCTAssertNotNil(task.date)
+    }
+
+}
