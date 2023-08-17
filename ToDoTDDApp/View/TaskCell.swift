321c3219d21f6a92a756282ed9ac8b5f7354613f
Zaki <menza.zm@gmail.com>
2023-08-17 16:05:42 +0600

    Number of rows in doneTasks section and cell for row return Task cell tests done

---
+++ ToDoTDDApp/View/TaskCell.swift
@@ -0,0 +1,12 @@
+//
+//  TaskCell.swift
+//  ToDoTDDApp
+//
+//  Created by Zaki on 17.08.2023.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+}
--- ToDoTDDAppTests/Controller/DataProviderTests.swift
+++ ToDoTDDAppTests/Controller/DataProviderTests.swift
@@ -15,8 +15,10 @@ final class DataProviderTests: XCTestCase {
 
     override func setUpWithError() throws {
         sut = DataProvider()
+        sut.taskManager = TaskManager()
         tableView = UITableView()
         tableView.dataSource = sut
+        
     }
 
     override func tearDownWithError() throws {
@@ -29,8 +31,7 @@ final class DataProviderTests: XCTestCase {
         XCTAssertEqual(numberOfSections, 2)
     }
     
-    func testNumberOfRowsInSectionZeroIsTaskCount() {
-        sut.taskManager = TaskManager()
+    func testNumberOfRowsInSectionZeroIsTasksCount() {
         sut.taskManager?.add(task: Task(title: "Bar"))
         
         XCTAssertEqual(tableView.numberOfRows(inSection: 0), 1)
@@ -40,6 +41,28 @@ final class DataProviderTests: XCTestCase {
         
         XCTAssertEqual(tableView.numberOfRows(inSection: 0), 2)
         
+    }
+    
+    func testNumberOfRowsInSectionOneIsDoneTasksCount() {
+        sut.taskManager?.add(task: Task(title: "Baz"))
+        sut.taskManager?.checkTask(at: 0)
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 1)
+        
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        sut.taskManager?.checkTask(at: 0)
+        tableView.reloadData()
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 2)
+    }
+    
+    func testCellForRowAtIndexPathReturnsTaskCell() {
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        tableView.reloadData()
+        
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0))
+        
+        XCTAssertTrue(cell is TaskCell)
     }
 
 }
