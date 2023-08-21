33019fd43b3ace603d5e4d430f3e5498a3e746de
Zaki <menza.zm@gmail.com>
2023-08-21 14:56:26 +0600

    DetailViewController added, label existing tests done

---
+++ ToDoTDDApp/Controller/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  ToDoTDDApp
+//
+//  Created by Zaki on 21.08.2023.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var mapView: MKMapView!
+    
+}
